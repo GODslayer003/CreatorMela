193609d89bb5b627c153554462c4a23dee2bb9d0
Pranjal Kundliya <144585679+GODslayer003@users.noreply.github.com>
2026-06-30 11:44:21 +0530

    Delete server directory

--- server/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { dashboardController } from '../controllers/dashboardController.js';
-import { authenticate } from '../middlewares/auth.js';
-
-const router = Router();
-
-router.get('/stats', authenticate, dashboardController.getStats);
-
-export { router as dashboardRoutes };
